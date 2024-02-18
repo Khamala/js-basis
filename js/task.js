49afba0aa9c8d080eb6b9e83b98ef97ab595a798
Khamala <elektrikamegawolt@gmail.com>
2024-02-18 13:16:16 +0100

    in + task

---
+++ js/task.js
@@ -0,0 +1,45 @@
+//                 РЕПЕТА Модуль 2, затятие 4 ФУНКЦИИ
+
+/*
+1. Напиши ф - цию calculateTotalPrice(item), которая принимает масив цен(чисел) и возвращает их сумму.
+
+const calculateTotalPrice = function (items) {
+    let total = 0;
+    
+    for (let item of items) {
+        total += item;
+    };
+    return total;
+};
+
+const cart = [52, 84, 96, 45, 36, 23, 78, 101, 7, 16];
+console.log(calculateTotalPrice(cart));
+console.log(calculateTotalPrice([1,2,3]));
+*/
+
+
+
+/*
+// 2. Напиши ф-цию  findLogin(logins, login) для поиска логина.  
+// - Если логина нет, то вывести сообщение: “Пользователь <login> не найден”. 
+// - Если нашли логин, то вывести сообщение: “Пользователь <login> найден”. 
+
+const logins = ['fgfgbhgf', 'dthdhfhfgh', 'rdthfh', 'ggg', '10px'];
+const loginToFind = 'ggg';
+
+const message = (logins, login) => {
+    return logins.includes(login)
+    ? `Пользователь ${login} найден`
+    : `Пользователь ${login} найден`
+};
+
+console.log(message(logins, loginToFind));
+*/
+
+
+/*
+// 3.	Напиши ф-цию findSmallesNumber(number) для поиска самого маленького числа в массиве чисел.
+
+
+
+*/
